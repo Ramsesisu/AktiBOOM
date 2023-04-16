e20c463d93058f0fc7213c115975515cd15efc88
Ramses <100287499+Ramsesisu@users.noreply.github.com>
2023-04-16 03:47:13 +0200

    Create EncryptionUtils.java

---
+++ src/main/java/me/rqmses/aktiboom/utils/EncryptionUtils.java
@@ -0,0 +1,12 @@
+package me.rqmses.aktiboom.utils;
+
+import java.util.Base64;
+
+public class EncryptionUtils {
+    public static String encode(String string, int key) {
+        return string;
+    }
+
+    public static String decode(String string, int key) {
+    }
+}
